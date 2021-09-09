d10a90bb1f8b68d0049c3b234236a8d8e31822b5
SeanyCarol <seany@ice.ufjf.br>
2021-09-08 23:03:09 -0300

    Project created

---
+++ src/main/java/com/devsuperior/auladomain/AuladomainApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.auladomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuladomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuladomainApplication.class, args);
+	}
+
+}
